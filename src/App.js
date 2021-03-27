c846bacdc0cc58568e4f8d2d365c2f3f6a45a7a0
Fionnuala12 <fionnuala.wilson@btinternet.com>
2021-03-27 13:56:43 +0000

    Search Engine Added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import React from "react"; 
+import Dictionary from './Dictionary';
 import book from './book.webp';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
         <img src={book} className="Book-logo img-fluid" alt="book" /> <h1> Dictionary</h1>
       </header>
+      <main> 
+        <Dictionary />
+      </main>
+      <footer>
+        Coded by Fionnuala Wilson
+      </footer>
+      </div>
     </div>
   );
 }
