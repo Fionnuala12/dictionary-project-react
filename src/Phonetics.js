3293de41a268c5c870e7b48e6893e4072453e8d5
Fionnuala12 <fionnuala.wilson@btinternet.com>
2021-04-04 21:03:26 +0100

    Phonetic link updated
    The audio now opens up on a new web page

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Phonetics(props){
     return (
         <div className="Phonetics"> 
-        <a href = {props.phonetic.audio} target="_blank"> 
+        <a href = {props.phonetic.audio} rel="noreferrer" target="_blank"> 
         Listen..
         </a>{" "}
         {props.phonetic.text}
