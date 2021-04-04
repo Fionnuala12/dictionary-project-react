d5357adc32a35cb261b00c1f87a158121a2b7a66
Fionnuala12 <fionnuala.wilson@btinternet.com>
2021-04-04 20:16:22 +0100

    Synonyms added

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,17 +1,21 @@
 import React from "react"; 
+import Synonyms from './Synonyms';
 
 export default function Meaning(props){
     return(
         <div className="Meaning">
             <h3>{props.meaning.partOfSpeech}</h3>
 
-            {props.meaning.definitions.map(function(defintion, index){
+            {props.meaning.definitions.map(function(definition, index){
                 return (
                     <div key={index}> 
-                    <p>{defintion.definition}
+                    <p><strong>Definition:</strong>{definition.definition}
                     <br /> 
-                    <em>{defintion.example}</em></p>
+                    <strong>Example:</strong>
+                    <em>{definition.example}</em></p>
+                    <Synonyms synonyms={definition.synonyms} />
                     </div>
+                    
                 )
             })}
         </div>
